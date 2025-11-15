91010b974b8924bea0e65e999572420c6fe4e330
ahmed12v <ahnurmed16@gmail.com>
2025-11-15 20:05:38 +0200

    preRender-pronlem-solve

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,13 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { provideServerRendering, RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   },
   {
     path: 'prouductDeteils/:id', 
     renderMode: RenderMode.Server
   },
+  
 ];
