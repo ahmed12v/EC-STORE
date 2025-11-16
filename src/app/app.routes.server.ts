b3626c94cee0b0afa52852bf6e5a0d33a599f74f
ahmed12v <ahnurmed16@gmail.com>
2025-11-16 16:18:05 +0200

    preeeeeeeeeee

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,18 @@ export const serverRoutes: ServerRoute[] = [
     path: 'prouductDeteils/:id', 
     renderMode: RenderMode.Server
   },
+  {
+    path: 'prouduct',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'new-pass',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'register',
+    renderMode: RenderMode.Server
+  },
    {
     path: '**',
     renderMode: RenderMode.Prerender
