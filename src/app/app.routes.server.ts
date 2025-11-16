e40673f822c077ce5f7be003c2c4dd6cd6203728
ahmed12v <ahnurmed16@gmail.com>
2025-11-16 16:14:42 +0200

    serverPrerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,13 +1,14 @@
 import { provideServerRendering, RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Server
-  },
+ 
   {
     path: 'prouductDeteils/:id', 
     renderMode: RenderMode.Server
+  },
+   {
+    path: '**',
+    renderMode: RenderMode.Prerender
   },
   
 ];
