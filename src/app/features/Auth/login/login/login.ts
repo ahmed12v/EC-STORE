539b2436597c93d949a0ed08a6ac25aceea62ba5
Ahmed Sami <ahnurmed16@gmail.com>
2026-05-25 12:45:34 +0300

    any thing

--- src/app/features/Auth/login/login/login.ts
+++ src/app/features/Auth/login/login/login.ts
@@ -1,56 +1,69 @@
-import { CommonModule } from '@angular/common';
-import { Component, Injector, runInInjectionContext, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Loginservice } from '../../../../core/services/Auth/login';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { Loginform } from '../../../../core/interfaces/Auth/login';
-import { tap } from 'rxjs';
-import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import {
+  Component,
+  Injector,
+  runInInjectionContext,
+  signal,
+} from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { Loginservice } from "../../../../core/services/Auth/login";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { Loginform } from "../../../../core/interfaces/Auth/login";
+import { tap } from "rxjs";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   imports: [ReactiveFormsModule, CommonModule, RouterLink],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  templateUrl: "./login.html",
+  styleUrl: "./login.css",
 })
 export class Login {
+  constructor(
+    private _Loginservice: Loginservice,
+    private _injector: Injector,
+    private _Router: Router
+  ) {}
+  banSpinner = signal(false);
+  errorMsg = signal("");
 
-  constructor(private _Loginservice:Loginservice , private _injector:Injector , private _Router:Router){}
-  banSpinner =signal(false)
-  errorMsg =signal('')
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl("", [Validators.email, Validators.required]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.pattern(/^[A-Z][a-z0-9]{6,10}$/),
+    ]),
+  });
 
-  loginForm:FormGroup =new FormGroup({
-    email:new FormControl(''  , [Validators.email , Validators.required]),
-    password:new FormControl('' , [Validators.required , Validators.pattern(/^[A-Z][a-z0-9]{6,10}$/)])
-  })
-
-  loginNowSubmit(){
-    if(this.loginForm.valid){
-      this.banSpinner.set(true)
-      runInInjectionContext(this._injector , ()=>{
-        const respont = toSignal(this._Loginservice.loginNow(this.loginForm.value as Loginform).pipe(
-          tap({
-            next:res =>{
-              this.banSpinner.set(false)
-              localStorage.setItem('user-token',res.token)
-              this._Loginservice.UserDataAfterDecoded.next(res.token);
-              this._Router.navigate(['/home'])
-              //console.log(res);
-              
-            },
-            error:err=>{
-              this.banSpinner.set(false)
-              console.log(err);
-              this.errorMsg.set(err.error.message);
-              //console.log(this.errorMsg());
-              
-              
-            }
-          })
-        )
-      )
-      })
+  loginNowSubmit() {
+    if (this.loginForm.valid) {
+      this.banSpinner.set(true);
+      runInInjectionContext(this._injector, () => {
+        const respont = toSignal(
+          this._Loginservice.loginNow(this.loginForm.value as Loginform).pipe(
+            tap({
+              next: (res) => {
+                this.banSpinner.set(false);
+                localStorage.setItem("user-token", res.token);
+                this._Loginservice.UserDataAfterDecoded.next(res.token);
+                this._Router.navigate(["/home"]);
+                //console.log(res);
+              },
+              error: (err) => {
+                this.banSpinner.set(false);
+                console.log(err);
+                this.errorMsg.set(err.error.message);
+                //console.log(this.errorMsg());
+              },
+            })
+          )
+        );
+      });
     }
   }
-
 }
--- src/app/features/components/cart/cart/cart.ts
+++ src/app/features/components/cart/cart/cart.ts
@@ -1,134 +1,147 @@
-import { Component, inject, Injector, OnInit, runInInjectionContext, signal } from '@angular/core';
-import { CartService } from '../../../../core/services/components/cart-service';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { tap } from 'rxjs';
-import { quantity, Root } from '../../../../core/interfaces/components/cart';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { RouterLink } from '@angular/router';
+import {
+  Component,
+  inject,
+  Injector,
+  OnInit,
+  runInInjectionContext,
+  signal,
+} from "@angular/core";
+import { CartService } from "../../../../core/services/components/cart-service";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { tap } from "rxjs";
+import { quantity, Root } from "../../../../core/interfaces/components/cart";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
+import { RouterLink } from "@angular/router";
 @Component({
-  selector: 'app-cart',
+  selector: "app-cart",
   imports: [RouterLink],
-  templateUrl: './cart.html',
-  styleUrl: './cart.css',
+  templateUrl: "./cart.html",
+  styleUrl: "./cart.css",
 })
-export class Cart implements OnInit{
-ngOnInit(): void {
-  this.userCart()
-}
-//#region declare
-_cartServise=inject(CartService);
-loadSpin=signal(false);
-_injectoer=inject(Injector);
-userCartCome=signal<Root | null>(null);
-_toaster=inject(ToastrService)
-waitUpdate=signal(false)
-removeproductSpin=signal(false)
-clearUsercartSoin=signal(false)
-//#endregion
-
-//#region getUserCart
-userCart(){
-  this.loadSpin.set(true)
-  runInInjectionContext(this._injectoer,()=>{
-    const userCart = toSignal(this._cartServise.getUserCart().pipe(
-      tap({
-        next:res=>{
-          this.loadSpin.set(false)
-          this.userCartCome.set(res)
-          console.log(this.userCartCome());
-          
-        },
-        error:err=>{
-          this.loadSpin.set(false)
-          console.log(err);
-          
-        }
-      })
-    ))
-  })
-}
-//#endregion
-
-//#region updateCount
-counForm=new FormGroup({
-  count:new FormControl( '', Validators.required)
-})
-updateCountProduct(productId:string,count:any){
-      this.waitUpdate.set(true)
-     setTimeout(()=>{
-      this.waitUpdate.set(false);
-     },3000)
-  this.counForm.patchValue({count:count})
-  if(this.counForm.valid){
-    runInInjectionContext(this._injectoer,()=>{
-      const newCount = toSignal(this._cartServise.updateProductQuantityInCart(
-        this.counForm.value as quantity , productId).pipe(
-          tap({
-            next:res=>{
-                  this.userCart()
-                  this._toaster.success('Updated Success','', {
-                  toastClass: 'custom-toast toast-success',
-               }) 
-               console.log(res);
-               
-            },
-            error:err=>{
-              console.log(err);
-              
-            }
-          })
-        ))
-    })
+export class Cart implements OnInit {
+  ngOnInit(): void {
+    this.userCart();
   }
-}
-//#endregion
+  //#region declare
+  _cartServise = inject(CartService);
+  loadSpin = signal(false);
+  _injectoer = inject(Injector);
+  userCartCome = signal<Root | null>(null);
+  _toaster = inject(ToastrService);
+  waitUpdate = signal(false);
+  removeproductSpin = signal(false);
+  clearUsercartSoin = signal(false);
+  //#endregion
 
-
-//#region remove product
-    removeProduct(productId:string){
-      this.removeproductSpin.set(true)
-      runInInjectionContext(this._injectoer,()=>{
-        const removSignal = toSignal(this._cartServise.removeProductFrpmCart(productId).pipe(
+  //#region getUserCart
+  userCart() {
+    this.loadSpin.set(true);
+    runInInjectionContext(this._injectoer, () => {
+      const userCart = toSignal(
+        this._cartServise.getUserCart().pipe(
           tap({
-            next:res=>{
-               console.log(res);
-               this.removeproductSpin.set(false)
-               this._toaster.success('product removed' )
-               this.userCart()
+            next: (res) => {
+              this.loadSpin.set(false);
+              this.userCartCome.set(res);
+              console.log(this.userCartCome());
             },
-            error:err=>{
-                  console.log(err);
-                  this.removeproductSpin.set(false)
-                  this._toaster.error('error in removed')
-            }
+            error: (err) => {
+              this.loadSpin.set(false);
+              console.log(err);
+            },
           })
-        ))
-      })
-    }
-//#endregion
+        )
+      );
+    });
+  }
+  //#endregion
 
-//#region clearAll
-ClearAll(){
-      this.clearUsercartSoin.set(true)
-      runInInjectionContext(this._injectoer,()=>{
-        const clearSignal = toSignal(this._cartServise.clearUserCart().pipe(
+  //#region updateCount
+  counForm = new FormGroup({
+    count: new FormControl("", Validators.required),
+  });
+  updateCountProduct(productId: string, count: any) {
+    this.waitUpdate.set(true);
+    setTimeout(() => {
+      this.waitUpdate.set(false);
+    }, 3000);
+    this.counForm.patchValue({ count: count });
+    if (this.counForm.valid) {
+      runInInjectionContext(this._injectoer, () => {
+        const newCount = toSignal(
+          this._cartServise
+            .updateProductQuantityInCart(
+              this.counForm.value as quantity,
+              productId
+            )
+            .pipe(
+              tap({
+                next: (res) => {
+                  this.userCart();
+                  this._toaster.success("Updated Success", "", {
+                    toastClass: "custom-toast toast-success",
+                  });
+                  console.log(res);
+                },
+                error: (err) => {
+                  console.log(err);
+                },
+              })
+            )
+        );
+      });
+    }
+  }
+  //#endregion
+
+  //#region remove product
+  removeProduct(productId: string) {
+    this.removeproductSpin.set(true);
+    runInInjectionContext(this._injectoer, () => {
+      const removSignal = toSignal(
+        this._cartServise.removeProductFrpmCart(productId).pipe(
           tap({
-            next:res=>{
-               console.log(res);
-               this.clearUsercartSoin.set(false)
-               this._toaster.success('Your Cart empty now' )
-               this.userCart()
+            next: (res) => {
+              console.log(res);
+              this.removeproductSpin.set(false);
+              this._toaster.success("product removed");
+              this.userCart();
             },
-            error:err=>{
-                  console.log(err);
-                  this.clearUsercartSoin.set(false)
-                  this._toaster.error('error in cleared')
-            }
+            error: (err) => {
+              console.log(err);
+              this.removeproductSpin.set(false);
+              this._toaster.error("error in removed");
+            },
           })
-        ))
-      })
-    }
-//#endregion
+        )
+      );
+    });
+  }
+  //#endregion
 
+  //#region clearAll
+  ClearAll() {
+    this.clearUsercartSoin.set(true);
+    runInInjectionContext(this._injectoer, () => {
+      const clearSignal = toSignal(
+        this._cartServise.clearUserCart().pipe(
+          tap({
+            next: (res) => {
+              console.log(res);
+              this.clearUsercartSoin.set(false);
+              this._toaster.success("Your Cart empty now");
+              this.userCart();
+            },
+            error: (err) => {
+              console.log(err);
+              this.clearUsercartSoin.set(false);
+              this._toaster.error("error in cleared");
+            },
+          })
+        )
+      );
+    });
+  }
+  //#endregion
 }
